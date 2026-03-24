51b3f9d0c8f726bc9978ec45060f0359d16ae93a
tensei <908739+tnsicdr@users.noreply.github.com>
2026-03-24 00:12:04 -0400

    chore: add deploy action

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/service-log",
   plugins: [
     tanstackRouter({
       target: "react",
